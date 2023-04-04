d47b45a8313a12d30782bde063f506b9ca841761
Juanvi Quiles <juquiles@aidacanarias.com>
2023-04-04 13:09:36 +0200

    chore: Create project

---
+++ Password.Tests/PasswordValidatorShould.cs
@@ -0,0 +1,16 @@
+namespace Password.Tests
+{
+    public class PasswordValidatorShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
